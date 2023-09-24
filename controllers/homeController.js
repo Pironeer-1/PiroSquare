40916fce6f5f2d4f42b34a6d7418fcfab4f846ef
hasoleee <wsw3727@gmail.com>
2023-09-25 02:21:12 +0900

    #5 fix: 충돌 해결

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,10 @@
 const homeModel = require('../models/homeModel.js');
 
 module.exports = {
+    
     getPosts: async (req,res) =>{
         const posts = await homeModel.home();
+        const users = await homeModel.getTest();
         res.render('home.ejs', {posts: posts});
     },
 }
--- models/homeModel.js
+++ models/homeModel.js
@@ -1,6 +1,14 @@
 const db = require('../config/db.js');
 
 module.exports = {
+
+    getTest: async ()=>{
+        const query = 'SELECT * FROM User;';
+        const users = await db.query(query);
+
+        return users[0];
+    },
+
     home: async () => {
         const query = 'SELECT * FROM Post'
         const posts = await db.query(query);
