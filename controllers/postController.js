1279bfc2b586e10e3f8f78aea1f6258cceca449a
hasoleee <wsw3727@gmail.com>
2023-09-27 02:03:45 +0900

    #34 refactor: 폴더 생성

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,19 +5,19 @@ module.exports = {
     // 자유 게시판 모든 글 불러오기
     getAll: async(req, res) =>{
         const posts = await postModel.getAll();
-        res.render('post.ejs', {posts: posts});
+        res.render('post/post.ejs', {posts: posts});
     },
 
     detailPost: async (req, res) =>{
         const postId=req.params.post_id
         const post = await postModel.detail(postId);
         const posts = await postModel.getAll();
-        res.render('postDetail.ejs', {posts: posts ,post: post});
+        res.render('post/postDetail.ejs', {posts: posts ,post: post});
     },
     
     createPost: async (req, res) =>{
         const posts = await postModel.getAll();
-        res.render('postCreate.ejs', {posts: posts});
+        res.render('post/postCreate.ejs', {posts: posts});
     },
 
     createNewPost: async (req, res) =>{
@@ -36,7 +36,7 @@ module.exports = {
         const postId=req.params.post_id
         const post = await postModel.detail(postId);
         const posts = await homeModel.home();
-        res.render('postUpdate.ejs', {posts: posts ,post: post});
+        res.render('post/postUpdate.ejs', {posts: posts ,post: post});
     },
     updateNewPost: async (req, res) =>{
         const postId=req.params.post_id;
