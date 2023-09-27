629bdbb306759014219e767025006d61dc6839ed
hasoleee <wsw3727@gmail.com>
2023-09-27 16:19:35 +0900

    #34 feat: 게시판 분리 및 구현

---
+++ controllers/recruitController.js
@@ -0,0 +1,48 @@
+const homeModel = require('../models/homeModel.js');
+const recruitModel = require('../models/recruitModel.js');
+
+module.exports = {
+
+    getAll: async(req, res) =>{
+        const posts = await recruitModel.getAll();
+        res.render('recruit/recruit.ejs', {posts: posts});
+    },
+
+    detail: async (req, res) =>{
+        const postId=req.params.post_id
+        const post = await recruitModel.detail(postId);
+        const posts = await recruitModel.getAll();
+        res.render('recruit/recruitDetail.ejs', {posts: posts ,post: post});
+    },
+
+    createRecruit: async (req, res) =>{
+        const posts = await recruitModel.getAll();
+        res.render('recruit/recruitCreate.ejs', {posts: posts});
+    },
+
+    createNewRecruit: async (req, res) =>{
+        const newPostData = req.body;
+        const insertId = await recruitModel.createNewRecruit(newPostData);
+        res.redirect(`/recruit/detail/${insertId}`);
+    },
+
+    deleteRecruit: async (req, res) =>{
+        const postId = req.params.post_id;
+        await recruitModel.deleteRecruit(postId);
+        res.redirect(`/recruit`);
+    },
+
+    updateRecruit: async (req, res) =>{
+        const postId=req.params.post_id
+        const post = await postModel.detail(postId);
+        const posts = await homeModel.home();
+        res.render('recruit/recruitUpdate.ejs', {posts: posts ,post: post});
+    },
+    updateNewRecruit: async (req, res) =>{
+        const postId=req.params.post_id;
+        const newPostData = req.body;
+        await recruitModel.updateRecruit(postId, newPostData);
+        res.redirect(`/recruit/detail/${postId}`);
+    },
+
+}
---
+++ routers/recruitRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const recruitController = require('../controllers/recruitController.js');
+
+router.get('/', recruitController.getAll);
+router.get('/detail/:post_id', recruitController.detail);
+router.get('/create', recruitController.createRecruit);
+router.post('/create', recruitController.createNewRecruit);
+router.post('/delete/:post_id', recruitController.deleteRecruit);
+router.get('/update/:post_id', recruitController.updateRecruit);
+router.post('/update/:post_id', recruitController.updateNewRecruit);
+
+module.exports = router;
