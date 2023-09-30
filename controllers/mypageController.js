34a44a78e8fb3941133d8399df0d077e3a736a2f
ggom121@naver.com <ggom121@naver.com>
2023-10-01 02:50:58 +0900

    #65 feat: 마이페이지 작성한 게시글, 좋아요한 게시글, 댓글남긴 게시글 기능 추가

--- controllers/mypageController.js
+++ controllers/mypageController.js
@@ -1,8 +1,14 @@
 const mypageModel = require('../models/mypageModel.js');
 module.exports = {
-    getUserInfo: async (req, res) => {
-        const user = await req.user
-        res.render('mypage/mypage.ejs', {user: user});
+    getInfo: async (req, res) => {
+        const user = await req.user;
+        const userPosts = await mypageModel.userPosts(user);
+        const likePosts = await mypageModel.likePosts(user);
+        const commentPosts = await mypageModel.commentPosts(user);
+        console.log('userPosts', userPosts[0]);
+        console.log('likePosts', likePosts[0]);
+        console.log('commentPosts', commentPosts[0]);
+        res.render('mypage/mypage.ejs', {user: user, userPosts: userPosts[0], likePosts: likePosts[0], commentPosts: commentPosts[0]});
     },
     updateUser: async (req, res) => {
         const user = await req.user
@@ -14,5 +20,5 @@ module.exports = {
         const imagePath = req.file ? `/user/${req.file.filename}` : '';
         await mypageModel.updateNewUser(user, updateUser, imagePath);
         res.redirect('/mypage');
-    }
+    },
 }
--- routers/mypageRouter.js
+++ routers/mypageRouter.js
@@ -19,7 +19,7 @@ const router = express.Router();
 
 const mypageController = require('../controllers/mypageController.js');
 
-router.get('/', mypageController.getUserInfo);
+router.get('/', mypageController.getInfo);
 router.get('/updateUser', mypageController.updateUser);
 router.post('/updateUser/:user_id', upload.single('image'), mypageController.updateNewUser);
 
