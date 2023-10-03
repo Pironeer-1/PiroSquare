8d4efe28c81e7d36abf737a8f398521f3b1f9092
ggom121@naver.com <ggom121@naver.com>
2023-10-03 21:14:17 +0900

    #92 fix: 로그인 연결 수정

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,12 +1,12 @@
 const apiModel = require('../models/apiModel.js');
 
 module.exports = {
-    getUser: async (req,res) => {
-        if (!req.session.passport,user) {
+    getUser: async (req, res) => {
+        if (!req.session || !req.session.passport || !req.session.passport.user) {
             res.status(400).send({ data: null, message: 'not authorized' });
         } else {
             const user = await req.user;
-            res.json({data: user, message: 'ok'});
+            res.json({ data: user, message: 'ok' });
         }
     },
 }
