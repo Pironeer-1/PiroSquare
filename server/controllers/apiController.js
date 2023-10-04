38502c29693300b7d4100da74a42fb0cef56f863
ggom121@naver.com <ggom121@naver.com>
2023-10-04 21:36:00 +0900

    #108 feat: is_active 링크 추가

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -2,6 +2,7 @@ const apiModel = require('../models/apiModel.js');
 
 module.exports = {
     getUser: async (req, res) => {
+        console.log('req.session', req.session);
         if (!req.session || !req.session.passport || !req.session.passport.user) {
             res.status(400).send({ data: null, message: 'not authorized' });
         } else {
--- server/controllers/passportController.js
+++ server/controllers/passportController.js
@@ -53,14 +53,15 @@ module.exports = function (app) {
     "/naver/callback",
     passport.authenticate("naver", { failureRedirect: "/" }),
     (req, res) => {
+      req.session.user = req.user;
       console.log("res.user", req.user);
       console.log("res.user.newUser", req.user.newUser);
       res.cookie("sessionID", req.sessionID, { maxAge: 1000 * 60 * 60 * 24 });
       if (req.user && req.user.newUser) {
-        return res.redirect("/auth/newUser");
+        return res.redirect("http://localhost:3000/my-page/update");
       }
       // res.send();
-      res.json({ data: req.user, message: "ok" });
+      res.redirect("http://localhost:3000/");
     }
   );
 
